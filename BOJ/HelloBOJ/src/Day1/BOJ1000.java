b0628e1d4542c395333ebaf16408f810ee402013
yonglee-sudo <52433669+yonglee-sudo@users.noreply.github.com>
2025-05-14 20:53:22 +0900

    Add files via upload

---
+++ BOJ/HelloBOJ/src/Day1/BOJ1000.java
@@ -0,0 +1,14 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class BOJ1000 {
+    public static void main(String[] args) {
+        int A, B;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A + B);
+    }
+}
